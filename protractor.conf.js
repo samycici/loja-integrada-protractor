33cfe0b54f0fe3eb221936db4bfd0746a1ffee74
Samanta Cicilia <samanta.cicilia@concrete.com.br>
2017-01-23 23:13:07 -0200

    Acertando para gerar relatório em caso de falha

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,7 @@
 
 exports.config = {
   directConnect: true,
+  ignoreUncaughtExceptions: true,
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
 
@@ -22,8 +23,46 @@ exports.config = {
     'no-source': true
   },
 
+  beforeLaunch: function() {
+    setTimeout(function() {
+        browser.driver.executeScript(function() {
+            return {
+                width: window.screen.availWidth,
+                height: window.screen.availHeight
+            };
+        }).then(function(result) {
+            browser.driver.manage().window().setSize(result.width, result.height);
+        });
+    });
+
+  },
+
   onPrepare: function() {
     browser.ignoreSynchronization=true;
-  }
+  },
+
+  afterLaunch: function() {
+    var reporter = require('cucumber-html-reporter');
+
+    var options = {
+          theme: 'bootstrap',
+          jsonFile: 'results.json',
+          output: 'cucumber_report.html',
+          reportSuiteAsScenarios: true,
+          launchReport: true,
+          metadata: {
+              "App Version":"0.0.1",
+              "Test Environment": "STAGING",
+              "Browser": "Chrome  54.0.2840.98",
+              "Platform": "OSX",
+              "Parallel": "Scenarios",
+              "Executed": "Remote"
+          }
+      };
+
+      reporter.generate(options);
+
+  },
+
 
 };
